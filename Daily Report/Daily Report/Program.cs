9781020e5d862860bfd575c0280633aac9126919
delena671 <69768942+delena671@users.noreply.github.com>
2020-10-27 04:03:51 -0400

    Add files via upload

---
+++ Daily Report/Daily Report/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple Daily Report
+            //Console.WriteLine("The Tech Accademy");
+            //Console.WriteLine("Student Daily Report");
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your Name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What course are you on?");
+            //string courseName = Console.ReadLine();
+            //Console.WriteLine("The course you are on: " + courseName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What page number?");
+            //string pageNumber = Console.ReadLine();
+            //Console.WriteLine("You are on page number: " + pageNumber);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Do you need any help with anything? Please answer \"true\" or \"false");
+            //string needHelp = Console.ReadLine();
+            //Console.WriteLine("You need help: " +needHelp );
+            //Console.ReadLine();
+
+            //Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            //string positiveFeedback = Console.ReadLine();
+            //Console.WriteLine("Your feedback: " + positiveFeedback);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Is there any other feedback you'd like to provide?");
+            //string otherFeedback = Console.ReadLine();
+            //Console.WriteLine("Thanks for sharing, I will let them know: " + otherFeedback);
+            //Console.ReadLine();
+
+            //Console.WriteLine("How many hours did you study today?");
+            //string yourHours = Console.ReadLine();
+            //Console.WriteLine("Great, keep up the good work: " + yourHours);
+            //Console.ReadLine();
+            //Console.WriteLine("Let us know if you need help with anything else.");
+            //string moreHelp = Console.ReadLine();
+            //Console.WriteLine("Perfect! We will be looking forward to it: " + moreHelp);
+            //Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
