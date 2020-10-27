6c629aef35d8a323e808db2e33b03c0692a6e786
delena671 <69768942+delena671@users.noreply.github.com>
2020-10-27 04:14:20 -0400

    Add files via upload

---
+++ Account/Account/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remeber, we're \"accounting\" on you!");
+            Console.ReadLine();
+
+        }
+    }
+}
