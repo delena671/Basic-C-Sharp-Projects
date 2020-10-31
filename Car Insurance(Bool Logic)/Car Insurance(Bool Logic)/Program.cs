19b504b8d440ef594fd09ca2eaab7349d5aa4c0a
delena671 <69768942+delena671@users.noreply.github.com>
2020-10-31 01:59:52 -0400

    Add files via upload

---
+++ Car Insurance(Bool Logic)/Car Insurance(Bool Logic)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Bool_Logic_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            int noDUI = 0;
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            int minAge = 15;
+            bool isApplicantAge = yourAge > minAge;
+            int absoluteNoDui = 0;
+            bool isApplicantDUI = noDUI == absoluteNoDui;
+            int minSpeedingTickets = 3;
+            bool isApplicantSTicket = input1 < minSpeedingTickets;
+            Console.WriteLine("True");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
