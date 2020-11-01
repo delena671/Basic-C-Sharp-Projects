688a85122252ec4878f3998fb69f8b9e657c2b72
delena671 <69768942+delena671@users.noreply.github.com>
2020-10-31 23:32:36 -0400

    Add files via upload

---
+++ String Functions/String Functions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    string name = "Adeline";
+        //    string quote = "The man said, \"Hello\", Adeline. \n Hello on a new line. \n \t Hello on a tab.";
+        //    string month = "November";
+        //    //Console.WriteLine(name);
+        //    //Console.WriteLine(quote);
+        //    //Console.WriteLine(month);
+        //    //Console.ReadLine();
+
+        //    Console.WriteLine("Adeline");
+        //    name = name.ToUpper();
+        //    Console.ReadLine();
+
+            //bool trueOrFalse = name.Contains("e");
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            
+            sb.Append("My name is Adeline");
+            
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
