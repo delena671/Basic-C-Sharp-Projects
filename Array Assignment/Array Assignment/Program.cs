90f9c17d062f44913dce3cdcefb64b75ef6d9711
delena671 <69768942+delena671@users.noreply.github.com>
2020-11-01 16:12:37 -0500

    Add files via upload

--- Array Assignment/Array Assignment/Program.cs
+++ Array Assignment/Array Assignment/Program.cs
@@ -7,18 +7,25 @@ namespace Array_Assignment
     {
         static void Main(string[] args)
         {
-            string firstName = "Adeline";
-            string greeting = "Hey";
 
+            string[] names = { "Adeline", "Joshua", "David", "Daniel" };
+            string input1 = Console.ReadLine();
 
-            List<string> intList = new List<string>();
-            intList.Add("Hey");
-            intList.Add("Adeline");
-            //intList.Remove("Adeline");
-            Console.WriteLine(intList[0]);
-            Console.ReadLine();
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (input1 == names[i])
+                {
+                    Console.WriteLine("Match found at index " + i);
+                }
+                else
+                {
+                    Console.WriteLine("User input does not match this item in the array.");
+                }
+            }
 
             int[] numArray = new int[10];
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
             numArray[0] = 5;
             numArray[1] = 2;
             numArray[2] = 10;
@@ -38,7 +45,8 @@ namespace Array_Assignment
             Console.ReadLine();
 
             List<string> colors = new List<string>() { "Pink", "Purple", "Lavender", "Lilac", "Blues" };
-            
+            string userColor = Console.ReadLine();
+
             foreach (string color in colors)
             {
                 Console.WriteLine(color);
