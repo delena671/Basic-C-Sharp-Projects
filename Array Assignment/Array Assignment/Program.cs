bf5651308dcc25f085b64456ad2b10721d0971f4
delena671 <69768942+delena671@users.noreply.github.com>
2020-11-01 19:33:53 -0500

    Add files via upload

--- Array Assignment/Array Assignment/Program.cs
+++ Array Assignment/Array Assignment/Program.cs
@@ -7,15 +7,16 @@ namespace Array_Assignment
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Write a name");
+            Console.ReadLine();
             string[] names = { "Adeline", "Joshua", "David", "Daniel" };
-            string input1 = Console.ReadLine();
+            int input1 = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < names.Length; i++)
             {
-                if (input1 == names[i])
+                if (input1 == i)
                 {
-                    Console.WriteLine("Match found at index " + i);
+                    Console.WriteLine("Match found at index " + names[i]);
                 }
                 else
                 {
@@ -23,33 +24,47 @@ namespace Array_Assignment
                 }
             }
 
-            int[] numArray = new int[10];
-            int input2 = Convert.ToInt32(Console.ReadLine());
-
+            Console.WriteLine("Pick a number");
+            Console.ReadLine();
+            int pickANumber = Convert.ToInt32(Console.ReadLine());
+            int[] numArray = new int[5];
             numArray[0] = 5;
-            numArray[1] = 2;
-            numArray[2] = 10;
-            numArray[3] = 200;
-            numArray[4] = 5000;
+            numArray[1] = 10;
+            numArray[2] = 20;
+            numArray[3] = 30;
+            numArray[4] = 40;
+            
+            int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
 
-            int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
-
-            int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+            int[] numArray2 = { 5, 2, 10, 200, 5000 };
 
             numArray2[5] = 650;
 
             Console.WriteLine(numArray2[3]);
             Console.ReadLine();
 
-            Console.WriteLine("The index you chose does not exist.");
+            for (int j = 0; j < numArray.Length; j++)
+            {
+                if (pickANumber == j)
+                {
+                    Console.WriteLine("Match found at index " + numArray[j]);
+                }
+                else
+                {
+                    Console.WriteLine("User input does not match this item in the array.");
+                }
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Choose a color");
             Console.ReadLine();
 
             List<string> colors = new List<string>() { "Pink", "Purple", "Lavender", "Lilac", "Blues" };
             string userColor = Console.ReadLine();
 
-            foreach (string color in colors)
+            foreach (string i in colors)
             {
-                Console.WriteLine(color);
+                    Console.WriteLine(i);
             }
             Console.ReadLine();
 
