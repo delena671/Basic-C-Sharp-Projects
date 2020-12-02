e92e243e8c1790d55760e553f64bbd3362b81702
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-01 19:41:39 -0500

    Add files via upload

---
+++ CMath223/CMath223/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMath223
+{
+    public class Card
+    {
+        public Card()
+        {
+            Suit = "Red";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
---
+++ CMath223/CMath223/Deck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMath223
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>() 
+            { 
+                "Red", "Yellow", "Green", "Blue", "Black",
+                "Switch", "Pick Two" 
+            };
+            List<string> Faces = new List<string>()
+            {
+                "Zero", "One", "Two", "Three", "Four", "Five", "Six", 
+                "Seven","Eight", "Nine", "Ten",
+            };
+            foreach (string face in Faces)
+            {
+                foreach (string suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
+
+        }
+    }
+}
