7433ccc624f734da1dcc828191645bd28aa36fb6
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-01 21:20:36 -0500

    Add files via upload

---
+++ AssignmentPG223/AssignmentPG223/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG223
+{
+    public class Class1
+    {
+        public int Math1(int x)
+        {
+            int result = x + 12;
+            return result;
+        }
+
+        public int Math2(int x)
+        {
+            int result = x - 6;
+            return result;
+        }
+
+        public int Math3(int x)
+        {
+            int result = x + 25;
+            return result;
+        }
+    }
+
+}
---
+++ AssignmentPG223/AssignmentPG223/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 classObject = new Class1();
+
+            Console.WriteLine("Choose any number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(classObject.Math1(input));
+            Console.ReadLine();
+
+            Console.WriteLine("Choose another number");
+            int inputTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(classObject.Math2(inputTwo));
+            Console.ReadLine();
+
+            Console.WriteLine("How about another number.");
+            int inputThree = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(classObject.Math3(inputThree));
+            Console.ReadLine();
+            
+        }
+    }
+}
