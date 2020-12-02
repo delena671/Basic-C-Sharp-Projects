c6ce2e1316dfc5b93a377d227490a0215109484e
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-02 16:34:37 -0500

    Add files via upload

---
+++ AssignmentPG226/AssignmentPG226/MathClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG226
+{
+    public class MathClass
+    {
+        public int Method1 (int x)
+        {
+            int result = x * 5;
+            return result;
+        }
+
+        public int Method2(decimal x)
+        {
+            decimal result = x / 65;
+            int num = Convert.ToInt32(result);
+            return num;
+        }
+
+        public int Method3(string x)
+        { 
+            int num = Convert.ToInt32(x);
+            int prod = num + 5;
+            return prod;
+        }
+    }
+}
---
+++ AssignmentPG226/AssignmentPG226/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass classObj = new MathClass();
+            //Method1
+            Console.WriteLine("Enter any number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(classObj.Method1(input));
+            Console.ReadLine();
+            //End
+
+            //Method2
+            Console.WriteLine("Enter another number.");
+            decimal input2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(classObj.Method2(input2));
+            Console.ReadLine();
+            //End
+
+            //Method3
+            Console.WriteLine("Final number.");
+            string input3 = Console.ReadLine();
+            Console.WriteLine(input3);
+            Console.ReadLine();
+
+            //End
+        }
+    }
+}
