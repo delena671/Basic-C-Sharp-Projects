b021de8fcce94373ede51862b877e5a9fbb8be7d
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-02 19:03:33 -0500

    Add files via upload

---
+++ AssignmentPG227/AssignmentPG227/Money.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG227
+{
+    public class Money
+    {
+        public int Method1(int x)
+        {
+            int result = x * 5;
+            return result;
+        }
+        public int Method2(int x)
+        {
+            int result = x + 20;
+            return result;
+        }
+    }
+}
---
+++ AssignmentPG227/AssignmentPG227/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssignmentPG227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money classObj = new Money();
+
+            //Method1
+            Console.WriteLine("Please enter any two numbers (One at a time), you may not need to enter a second number if you like.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(classObj.Method1(input));
+            Console.ReadLine();
+        }
+    }
+}
