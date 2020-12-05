44911228655cce5d14712e86cc9a8ced1ee484d8
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-04 19:17:14 -0500

    Add files via upload

---
+++ AssignmentPG228/AssignmentPG228/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG228
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Void classAddV = new Void();
+        }
+    }
+}
---
+++ AssignmentPG228/AssignmentPG228/Void.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG228
+{
+    public class Void
+    {
+        public int AddingTwoNumbers(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
