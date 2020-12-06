580e13d2e9db64f1b5048ddec7b2bc997bbfaf10
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-06 10:45:19 -0500

    Add files via upload

---
+++ AssignmentPG231/AssignmentPG231/Class31.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG231
+{
+    public class Class31
+    {
+        public void getValue(int x)
+        {
+            int result = x / 2;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ AssignmentPG231/AssignmentPG231/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG231
+{
+    class Program
+    {
+        public void getAnswers(out int x)
+        {
+            int temp = 5;
+            x = temp;
+        }
+        
+        static void Main(string[] args)
+        {
+            Class31 classOut = new Class31();
+            Console.WriteLine("Please enter a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            classOut.getValue(input);
+            Console.ReadLine();
+
+        }
+
+        public static void Add(string x, string y)
+        {
+            string answer = x + " " + y;
+            Console.WriteLine(answer);
+        }
+    }
+}
