49d791ef7ce0559fbac710644dcb6a1543c47b21
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-06 14:09:21 -0500

    Add files via upload

--- AssignmentPG231/AssignmentPG231/Program.cs
+++ AssignmentPG231/AssignmentPG231/Program.cs
@@ -11,13 +11,13 @@ namespace AssignmentPG231
         static void Main(string[] args)
         {
             Class31 classOut = new Class31();
+            classOut.Add(x, y);
+            classOut.getAnswers(x);
+
             Console.WriteLine("Please enter a number.");
             int input = Convert.ToInt32(Console.ReadLine());
             classOut.getValue(input);
             Console.ReadLine();
-
-            classOut.Add(input);
-            Class31.getAnswers(input);
            
         }
     }
