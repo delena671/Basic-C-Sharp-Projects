236f6376740cd144120a2a1db329d356416c55af
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-07 17:53:05 -0500

    Add files via upload

---
+++ AssignmentPG236/AssignmentPG236/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG236
+{
+    public class Person
+    {
+        public Method1(string = "FirstName", "LastName")
+        {
+            public List<string> FullName { get; set; }
+        public string FisrtName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            foreach (string name in FullName)
+            {
+                Console.WriteLine("Name:" + "FirstName" + "LastName");
+            }
+        }
+    }
+}
+}
---
+++ AssignmentPG236/AssignmentPG236/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG236
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person People = new Person();
+
+            Employee person = new Employee();
+
+            Console.WriteLine("What is your name");
+            People.Method1("FisrtName", "LastName"); //This begins with the OBJ 
+            //name "People" because it is not static.
+            string name = 
+            Person.FulName("FisrtName, LastName"); //This begins with the class 
+            //name "Person" because it is static.
+            Console.ReadLine();
+        }
+    }
+}
