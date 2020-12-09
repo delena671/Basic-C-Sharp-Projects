29467618e11747f54acde929d5d614845a1bf29a
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-08 20:07:00 -0500

    Add files via upload

---
+++ AssignmentPG244/AssignmentPG244/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG244
+{
+    public abstract class Person
+    {
+        string firstName { get; set; }
+        string lastName { get; set; }
+        
+        public abstract void SayName();  
+    }
+}
