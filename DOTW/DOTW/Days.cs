acb066e217bddbcc73c0d75bb51086d1f5910e80
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-11 22:33:25 -0500

    Add files via upload

---
+++ DOTW/DOTW/Days.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTW
+{
+    public class Days
+    {
+        public Everyday Everyday { get; set; }
+    }
+    public enum Everyday
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ DOTW/DOTW/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is today?");
+            Everyday day = Everyday.Friday;
+
+            try
+            {
+                string Today = day;
+                Console.WriteLine(Today);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.WriteLine("The try/catch is finished, and today is friday.");
+            }
+        }
+
+    }
+}
