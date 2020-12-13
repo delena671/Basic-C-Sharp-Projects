00ca566d43b66893d7c2cf5cae5c270f623bd12c
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-12 23:36:05 -0500

    Add files via upload

---
+++ 123struct/123struct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _123struct
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number amountObj = new Number();
+            amountObj.Amount = 32.5m;
+            Console.WriteLine(amountObj.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
