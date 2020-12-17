89f3352f5d43296e8c2c21124ee5d5b15c887bbc
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-16 19:20:22 -0500

    Add files via upload
    I also went a head and up loaded my log.txt

---
+++ Logs/Logs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            //string text = "input";
+            //File.WriteAllText(@"C:\Users\riseu\source\repos\log.txt", text);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\riseu\source\repos\log.txt", true))
+            {
+                file.WriteLine(input);
+            }
+            Console.ReadLine();
+        }
+    }
+}
