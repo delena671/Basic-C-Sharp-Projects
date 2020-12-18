24cc6eb3aa24a32ac12a8b084bd78a956cd6bebb
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-18 11:20:53 -0500

    Add files via upload

--- Logs/Logs/Program.cs
+++ Logs/Logs/Program.cs
@@ -15,13 +15,15 @@ namespace Logs
             int input = Convert.ToInt32(Console.ReadLine());
             //string text = "input";
             //File.WriteAllText(@"C:\Users\riseu\source\repos\log.txt", text);
+            DateTime dateTime = new DateTime(2020, 12, 18, 10, 35, 30);
             using (StreamWriter file = new StreamWriter(@"C:\Users\riseu\source\repos\log.txt", true))
-
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(input);
             }
             Console.WriteLine(File.ReadAllText(@"C:\Users\riseu\source\repos\log.txt"));
             Console.ReadLine();
         }
+
     }
 }
