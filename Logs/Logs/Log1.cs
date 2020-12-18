8caa019acbb848c18698613fb83dd54dd82f9879
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-18 18:55:45 -0500

    Add files via upload

---
+++ Logs/Logs/Log1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logs
+{
+    public class Log1
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Age { get; set; }
+        public Log1(string firstName, string lastName, int Age)
+        {
+            //First Constructor that passes firstName, lastName and Age
+        }
+        public Log1(string firstName, int Age): this (firstName, "John", Age )
+        {
+            //Second Constructor passes the firstName "John" and and Age
+            this.Age = Age;
+            Age = 30;
+        }
+    }
+}
