5a134741e40740806599974d2dca29a5dc5775c8
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-18 20:15:23 -0500

    Add files via upload

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace DateAndTime
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            DateTime dateTime = new DateTime ( 2020, 12, 18, 07, 45, 30 );
+            Console.WriteLine("Please enter a number.");
+            int user = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(dateTime);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\riseu\source\repos\DateTime\log.txt", true))
+            {
+                file.WriteLine(user);
+            }
+            Console.ReadLine();
+        }
+    }
+}
