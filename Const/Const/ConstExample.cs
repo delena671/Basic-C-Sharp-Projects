f9c0167bb858c84faf54263d4e3a23512341010e
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-18 20:38:46 -0500

    Add files via upload

---
+++ Const/Const/ConstExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const
+{
+    class ConstExample
+    {
+        const string Var = "Hello";
+
+        public int myAge { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public ConstExample(string firstName, string lastName, int myAge)
+        {
+            //First constructor
+        }
+        public ConstExample(string firstName, int myAge) : this(firstName, "Adeline", myAge)
+        {
+            //Second constructor
+            this.myAge = myAge;
+            myAge = 28;
+        }
+    }
+}
---
+++ Const/Const/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var varString = "How are you? I have more of my project in the ConstExample class.";
+            Console.WriteLine(varString);
+            Console.ReadLine();
+        }
+    }
+}
