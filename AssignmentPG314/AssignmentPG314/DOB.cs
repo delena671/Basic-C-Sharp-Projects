474b8568f761d54f54b7f925f8cfc37cfe72c4ed
delena671 <69768942+delena671@users.noreply.github.com>
2020-12-20 11:04:13 -0500

    Add files via upload

---
+++ AssignmentPG314/AssignmentPG314/DOB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPG314
+{
+    public class DOB
+    {
+        public int Year { get; set; }
+
+        public void Method1()
+        {
+
+        }
+    }
+}
---
+++ AssignmentPG314/AssignmentPG314/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AssignmentPG314
+{
+    class Program
+    {
+        public static void TryInt( int Try)
+        {
+            if(Try < 0)
+            {
+                throw new Exception();
+            }
+        }
+        public static void Main(string[] args)
+        {
+            DOB birthday = new DOB();
+            Console.WriteLine("Hello, let's start by telling me your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            DateTime yOB = DateTime.Now;
+            int year = yOB.Year - userAge;
+            Console.WriteLine(year);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number.");
+            try
+            {
+                string answer = Console.ReadLine();
+                int age = Int32.Parse(answer);
+                TryInt(age);      
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
